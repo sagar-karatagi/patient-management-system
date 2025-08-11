afce5e6e03f8e674f6e3a665f5d3269e76891aa6
sagar-karatagi <sagrkaratagi@gmail.com>
2025-08-11 17:32:14 +0530

    Deleted protos

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -1,21 +1,21 @@
 package com.pm.billingservice.grpc;
 
+
 import com.pm.billing.BillingRequest;
 import com.pm.billing.BillingResponse;
 import com.pm.billing.BillingServiceGrpc;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
-import net.devh.boot.grpc.server.service.GrpcService;
 
 
 @Slf4j
 @GrpcService
 public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+
     @Override
     public void createBillingAccount(BillingRequest request, StreamObserver<BillingResponse> responseObserver) {
         log.info("Create billing account {}", request);
-        BillingResponse response = BillingResponse
-                .newBuilder()
+        BillingResponse response = BillingResponse.newBuilder()
                 .setAccountId("1234")
                 .setStatus("SUCCESS")
                 .build();
@@ -23,4 +23,5 @@ public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBas
         responseObserver.onCompleted();
     }
 }
+
 
